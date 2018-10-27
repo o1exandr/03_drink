295ec2787b8b59d03138c45480b213dd225ee908
Oleksandr Finiuk <olexandr.finyuk@gmail.com>
2018-10-27 20:36:45 +0300

    done

--- 03_drink/Program.cs
+++ 03_drink/Program.cs
@@ -46,7 +46,7 @@ using System.Collections;
 
 namespace _03_drink
 {
-    public class Drink : IComparable
+    public class Drink : IComparable, IComparable<Drink>, IEquatable<Drink>
     {
 
         public string name;
@@ -69,7 +69,7 @@ namespace _03_drink
 
         public override string ToString()
         {
-            return $"{name}\t{type, 15}\t{manufacturer, 15}\t{energy, 10}\t{price}$";
+            return $"{name}\t{type, 15}\t{manufacturer, 15}\t{energy, 6}\t{price}$";
         }
 
         // Реалізувати інтерфейс  IComparable(як метод класу int CompareTo(object)) : порівнювати напої за типом, потім за назвою
@@ -83,7 +83,6 @@ namespace _03_drink
                 throw new FormatException("CompareTo(obj)-->obj should be Product type");
 
             Drink drink = (Drink)obj;
-            //return this.type.CompareTo(drink.type);
             if (this.type == drink.type)
             {
                 return this.name.CompareTo(drink.name);
@@ -97,6 +96,28 @@ namespace _03_drink
         public int CompareTo(Drink drink)
         {
             return this.name.CompareTo(drink.name);
+        }
+
+        // Реалізувати інтерфейс  IEquatable<>
+        public bool Equals(Drink other)
+        {
+            if (other is null)
+            {
+                return false;
+            }
+
+            if (object.ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
+            return name.Equals(other.name);
+        }
+
+        public override int GetHashCode()
+        {
+            int hashProductName = name?.GetHashCode() ?? 0;
+            return hashProductName;
         }
 
     };
@@ -118,6 +139,7 @@ namespace _03_drink
         }
     }
 
+    // Визначити 3 класи   компараторів(реалізують інтерфейс ICompare: тобто int Compare(object, object)) 
     // порівняння за ціною(за зростанням)
     class MyComparerPrice : IComparer
     {
@@ -150,12 +172,39 @@ namespace _03_drink
         }
     }
 
-    
+    // Визначити 3 класи компараторів(реалізують інтерфейс ICompare<Drink>: тобто int Compare(Drink, Drink)) 
+    // порівняння за ціною(за спаданням)
+    class MyComparerPriceGeneric : IComparer<Drink>
+    {
+        public int Compare(Drink one, Drink two)
+        {
+            return -one.price.CompareTo(two.price);
+        }
+    }
+    // порівняння за ккал(зростанням)
+    class MyComparerEnergyGeneric : IComparer<Drink>
+    {
+        public int Compare(Drink one, Drink two)
+        {
+            return one.energy.CompareTo(two.energy);
+        }
+    }
+    // порівняння за  виробником(зростання)
+    class MyComparerVendorGeneric : IComparer<Drink>
+    {
+        public int Compare(Drink one, Drink two)
+        {
+            return one.manufacturer.CompareTo(two.manufacturer);
+        }
+    }
+
+
     class Program
     {
         static public void PrintArr(ArrayList list, string message = "")
         {
             Console.WriteLine("\n\t{0}", message);
+            Console.WriteLine("Name\t\tType\t    Vendor\tEnergy\tPrice\n------------------------------------------------------");
             foreach (Drink dr in list)
                 Console.WriteLine(dr);
         }
@@ -163,6 +212,7 @@ namespace _03_drink
         static public void PrintList(List<Drink> list, string message = "")
         {
             Console.WriteLine("\n\t{0}", message);
+            Console.WriteLine("Name\t\tType\t    Vendor\tEnergy\tPrice\n------------------------------------------------------");
             foreach (Drink dr in list)
                 Console.WriteLine(dr);
         }
@@ -171,7 +221,7 @@ namespace _03_drink
         {
             Drink d = new Drink("IceTea", Drink.Kind.Tea, "Lipton", 25, 0.99);
             List<Drink> drinks = new List<Drink>
-            { d,
+            {   d,
                 new Drink("Coffee", Drink.Kind.Coffee, "Nescafe", 50, 1.99),
                 new Drink("Beer", Drink.Kind.Alcohol, "Leff", 100, 2.5),
                 new Drink("Water", Drink.Kind.MineralWater, "Borjomi", 5, 1.20),
@@ -194,21 +244,17 @@ namespace _03_drink
             list.Add(new Drink("Wiski", Drink.Kind.Alcohol, "Jack Daniel's", 120, 50));
             list.Add(d);
 
-            /*
-            Console.WriteLine("\n\t- Sort by energy -");
-            drinks.Sort((x, y) => x.energy.CompareTo(y.energy));
-            foreach (Drink dr in drinks)
-                Console.WriteLine(dr);
-                */
-            PrintArr(list, "- All drinks -");
+            PrintArr(list, "- All drinks unsort -");
 
-            
-            drinks.Sort((x, y) => { if (x.type == y.type) { return x.name.CompareTo(y.name); } else return x.type.CompareTo(y.type); });
-            PrintList(drinks, "\n\t- Sort by type/name -");
-            
+            // Реалізувати інтерфейс  IComparable(як метод класу int CompareTo(object)) : порівнювати напої за типом, потім за назвою
             list.Sort();
-            PrintArr(list, "- Sort by name -");
+            PrintArr(list, "\n\t- Sort by type/name - (object)");
 
+            // Реалізувати інтерфейс  IComparable< >(як метод класу int CompareTo(Drink)) : порівнювати за назвою напою
+            drinks.Sort();
+            PrintList(drinks, "- Sort by name - (Drink)");
+
+            Console.WriteLine("\n - Compare: int Compare(object, object) - ");
             // порівняння за ккал(спаданням)
             list.Sort(new MyComparerEnergy());
             PrintArr(list, "- Sort by energy -");
@@ -219,7 +265,24 @@ namespace _03_drink
 
             // порівняння за  виробником(зростанням)
             list.Sort(new MyComparerManufacturer());
-            PrintArr(list, "- Sort by manufacturer -");
+            PrintArr(list, "- Sort by vendor -");
+
+            Console.WriteLine("\n - ICompare<Drink>: int Compare(Drink, Drink)) - ");
+            // порівняння за ціною(за спаданням)
+            drinks.Sort(new MyComparerPriceGeneric());
+            PrintList(drinks, "- Sort by price -");
+
+            // порівняння за ккал(зростанням)
+            drinks.Sort(new MyComparerEnergyGeneric());
+            PrintList(drinks, "- Sort by energy -");
+
+            // порівняння за  виробником(зростання)
+            drinks.Sort(new MyComparerVendorGeneric());
+            PrintList(drinks, "- Sort by vendor -");
+
+            // Реалізувати інтерфейс  IEquatable <>
+            int firstIndex = drinks.IndexOf(d); // Tea IceTea
+            Console.WriteLine($"\nIndex of drink {d.name} : {firstIndex}");
 
             Console.ReadKey();
 
